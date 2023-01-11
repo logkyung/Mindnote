f4746e9400f4e1be63904d29bdff56c63bbe815f
logkyung <aebong54@gmail.com>
2023-01-11 18:19:22 +0900

    Add update and delete function

--- diary/views.py
+++ diary/views.py
@@ -28,4 +28,24 @@ def page_create(request):
     else:
         form = PageForm()
     return render(request, 'diary/page_form.html', {"form":form})
-    
+
+
+def page_update(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        form = PageForm(request.POST, instance=object)
+        if form.is_valid():
+            form.save()
+            return redirect('page-detail', page_id=object.id)
+    else:
+        form = PageForm(instance=object)
+    return render(request, 'diary/page_form.html', {'form':form})
+
+
+def page_delete(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        object.delete()
+        return redirect('page-list')
+    else:
+        return render(request, 'diary/page_confirm_delete.html', {'object':object})
