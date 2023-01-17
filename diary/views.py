a87029bbcb6ff468519bd3c163645b4d3bc30c82
logkyung <aebong54@gmail.com>
2023-01-18 00:51:44 +0900

    Add pagination

--- diary/views.py
+++ diary/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from diary.models import Page
 from diary.forms import PageForm
 
 # Create your views here.
 def page_list(request):
     object_list = Page.objects.all()
-    context = {"object_list":object_list}
-    return render(request, 'diary/page_list.html', context=context)
+    paginator = Paginator(object_list, 8)
+    curr_page_num = request.GET.get('page')
+    if curr_page_num is None:   #만약 쿼리스트링에 'page'가 없어서 None이 설정되었다면
+        curr_page_num = 1       # 첫 번째 페이지를 가리키는 1로 바꿔준다.
+    page = paginator.page(curr_page_num)
+    return render(request, 'diary/page_list.html', {'page':page})
 
 
 def page_detail(request, page_id):
